c802b999d96598bbac8919e9df430ffa2dd9b738
Harshi-13 <harshi.satish13@gmail.com>
2022-03-30 00:02:34 +0530

    clear23

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.querySelector('#push').onclick = function(){
         }
 
         //var tasks = document.querySelectorAll("task1");
-        var tasks = document.getElementById("strike").value;
+        var tasks = document.getElementById("strike");
         alert(tasks);
         for(var i=0; i<tasks.length; i++){
             tasks[i].onclick = function(){
