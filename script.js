5a4358ce26d9219ef12dfcd043418a582487c72f
Harshi-13 <harshi.satish13@gmail.com>
2022-03-30 08:26:01 +0530

    strw24

--- script.js
+++ script.js
@@ -67,14 +67,14 @@ document.querySelector('#clearall').onclick = function(){
 
 }
 
-document.querySelector('.task1').onclick = function(){ {
+document.querySelector('.task1').onclick = function(){
 	alert("S2");
 	 var current_tasks = document.querySelectorAll(".task1");
         for(var i=0; i<current_tasks.length; i++){
             current_tasks[i].onclick = function(){
 				 	document.querySelector(".task2").style.display="block";
 					document.querySelector(".task2").style.visibility="visible";
-				//	document.querySelector(".task2").style.alignItems="center";
+					document.querySelector(".task2").style.alignItems="center";
 
 					document.querySelector(".task3").style.display="block";
 					document.querySelector(".task3").style.visibility="visible";
